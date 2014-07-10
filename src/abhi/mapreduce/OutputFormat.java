3e0498f42dc2932fed312003ad502b922e81d7d5
Abhishek Sharma <abhisharma@cmu.edu>
2014-07-09 20:26:54 -0400

    Added the base class for the output format

--- src/abhi/mapreduce/OutputFormat.java
+++ src/abhi/mapreduce/OutputFormat.java
@@ -8,9 +8,20 @@ package abhi.mapreduce;
  *
  * OutputFormat describes the output-specification for a Map-Reduce job.
  * 
- * Provided with the key and value this method should return a String that must be written to the Output of the Map-Reduce Job
+ * Two main functions:
+ * 1. Provided with the key and value this method should return a String that must be written to the Output of the Map-Reduce Job
+ * 2. Check if the OUTPUT directory already exists
  */
 public abstract class OutputFormat<KOUT, VOUT> {
 
 	public abstract String format(KOUT key, VOUT value);
+	
+	//TODO: Abhi
+	//Implement code to check if the OUTPUT directory already exists. If yes then we cannot run the JOB 
+	//This is as feature of Hadoop
+	public void checkifOutputDirectoryExists()
+	{
+		
+	}
+	
 }
