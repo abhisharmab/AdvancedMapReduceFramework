e086c0e8224814f8f1360f4eb447260c8c2db06d
Abhishek Sharma <abhisharma@cmu.edu>
2014-07-12 18:12:15 -0400

    Test class for some stuff

---
+++ src/abhi/mapreduce/Test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package abhi.mapreduce;
+
+/**
+ * @author abhisheksharma
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int registryPort = Integer.parseInt(SystemConstants.getConfig(SystemConstants.REGISTRY_PORT));
+		int x = 0;
+	}
+
+}
