05aecaf87dc70709d07ea2a69cd9bc40cbc5b783
Abhishek Sharma <abhisharma@cmu.edu>
2014-07-12 13:49:51 -0400

    JobTracker Services Interface

---
+++ src/abhi/mapreduce/IJobTrackerServices.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package abhi.mapreduce;
+
+/**
+ * @author abhisheksharma
+ *
+ *This interface defines the services that the JobTracker must provide to JobClient, 
+ *TaskTracker etc. Basically it defines all the methods that handle communication with JobTrackers
+ *
+ *JobTracker would implement these methods and provide the appropriate services to the caller.
+ *
+ */
+public interface IJobTrackerServices {
+
+}
