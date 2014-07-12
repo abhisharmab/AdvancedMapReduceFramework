3723bb283c3603ddc1bc2398a309a73665b2b79a
Abhishek Sharma <abhisharma@cmu.edu>
2014-07-11 22:33:46 -0400

    Addimg JobConf

--- src/abhi/mapreduce/JobConf.java
+++ src/abhi/mapreduce/JobConf.java
@@ -18,9 +18,6 @@ import java.io.Serializable;
  */
 public class JobConf implements Serializable{
 
-	  /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	private int reducerNum;
