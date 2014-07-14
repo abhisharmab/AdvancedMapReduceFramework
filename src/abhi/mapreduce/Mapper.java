b8dc3e9a105550c0ac86a01300e0ed65ff2605ec
Abhishek Sharma <abhisharma@cmu.edu>
2014-07-14 11:19:30 -0400

    x

--- src/abhi/mapreduce/Mapper.java
+++ src/abhi/mapreduce/Mapper.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 
 public abstract class Mapper<KIN, VIN, KOUT, VOUT> {
 
-	//Called once at the beginning to setup the Map Task or perform some pre-processing as per Application Need
+	//Called once at the beginning to setup the Map Task or perform some preprocessing as per Application Need
 	protected void setup() throws IOException, InterruptedException {};
 	
 	//Called once at the end (Possibly for some cleanup and housekeeping work)
