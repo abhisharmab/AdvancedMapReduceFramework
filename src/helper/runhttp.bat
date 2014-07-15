93e6023855586e8ace573d1000706376c5a4f77f
Abhishek Sharma <abhisharma@cmu.edu>
2014-07-15 19:54:48 -0400

    Added the Helper HTTP function that we will use to download the code

---
+++ src/helper/runhttp.bat
@@ -0,0 +1,12 @@
+rem     This file will run a simple HTTP Server that only functions as a 
+rem     source for Java class files.
+rem
+rem     The first parameter is the TCP/IP port number on which this server
+rem     listens.
+rem
+rem     The second parameter is the local directory that contains the
+rem     Java class files to be served.
+
+java ClassFileServer 2002  D:\home\didattica\tiga\materiale-didattico\java\rmi\classfileserver\
+
+
